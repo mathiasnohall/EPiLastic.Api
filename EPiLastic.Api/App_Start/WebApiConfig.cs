817078e14cffc0a3349e062153470769e9e139fc
Mathias Nohall <mathias.nohall@valtech.se>
2016-07-19 16:00:13 +0200

    structuremap dependency injection

--- EPiLastic.Api/App_Start/WebApiConfig.cs
+++ EPiLastic.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using EpiLastic.Querying;
+using EpiLastic.Services;
+using EPiLastic.Api.Services;
+using EPiLastic.Api.StructureMap;
+using StructureMap;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 
 namespace EPiLastic.Api
 {
@@ -10,15 +13,20 @@ namespace EPiLastic.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var container = new Container();
+            container.Configure(c => c.For<ISearchResponseMapper>().Use<SearchResponseMapper>());
+            container.Configure(c => c.For<ISearchClient>().Use<SearchClient>());
+            container.Configure(c => c.For<ILoggerWrapper>().Use<LoggerWrapper>());            
+
+            config.Services.Replace(typeof(IHttpControllerActivator), new StructureMapControllerActivator(container));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            name: "SearchAPI",
+            routeTemplate: "search/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional });
         }
     }
 }
